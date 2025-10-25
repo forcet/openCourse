32a70e83b8e42345ef6ed935bdaaf6a104239386
Mario Correa <mario.correa@fisagrp.com>
2025-10-25 15:52:56 -0500

    fix: add log for url

--- src/service/LuckyService.ts
+++ src/service/LuckyService.ts
@@ -1,6 +1,7 @@
 import { Http } from "./Http";
 
 const url = process.env.NEW_SERVICE || "http://localhost:3000/lucky";
+console.log(`Url variable de entorno: ${url}`);
 
 export class LuckyService { 
     async getExternalService(): Promise<string> {
