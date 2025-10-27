1aca808da440a822f9cb00d16c31c09f8a0a6756
Mario Correa <mario.correa@fisagrp.com>
2025-10-27 18:37:21 -0500

    fix: add routes for service

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import genericRouter  from "./routes/Generic"
+import start  from "./routes/Start"
+import health  from "./routes/Health"
+import readiness  from "./routes/Readiness"
 
 const app = express();
 const port = process.env.APP_PORT || 3001;
 
 app.use(genericRouter);
+app.use(start);
+app.use(health);
+app.use(readiness);
 
 app.listen(port, () => {
   console.log(`Servidor TS escuchando en http://localhost:${port}`);
